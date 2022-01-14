9bb2a9fc70b08aac60829d674ebb978e07901bd3
Peter Thao <peterthao@Peters-iMac.local>
2022-01-13 20:26:37 -0500

    merging to pull develop

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 //import from npm packages
 import './App.css';
-<<<<<<< HEAD
 import { BrowserRouter as
   Router,
   Route,
@@ -27,62 +26,6 @@ function App() {
       </Switch>
     </Router>
     </div>
-=======
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { StoreProvider } from './utils/GlobalState';
-
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="App">
-          <StoreProvider>
-            <header className="App-header">
-              <p>
-                Vaccine Tracker!
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          </StoreProvider>
-        </div>
-      </Router>
-    </ApolloProvider>
->>>>>>> 5e351abd5da25ac467e38daa392a7fa79211c455
   );
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { EntryPage, PageHeader } from './style';
+
 import EntryCard from '../components/EntryCard';
 import InputGroup from '../components/InputGroup';
 import Input from '../components/Input';
 import Button from '../components/Button';
 
+import Auth from '../utils/auth';
+
+
+
 const Login = () => {
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            const mutationResponse = await Login({
+                variables: {email: fromState.email, password: formState.password }
+            })
+        }
+    }
 
     return (
         <EntryPage>
